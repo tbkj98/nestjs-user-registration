d1e73420f57e00519983b02f3f16539a9bb4f7d1
Anand Sharma <anandsharma334@gmail.com>
2021-06-05 22:09:09 +0530

    Logged database URL

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -7,11 +7,13 @@ import { UserEntity } from '../../entity/user.entity';
 import { UserModule } from '../user/user.module';
 import { PasswordResetEntity } from 'src/entity/reset.entity';
 
+const dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://tbkj:Anand%40334@192.168.1.43:5432/storeman"
+console.error("Database URL", dbUrl);
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRoot({
-      url: process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://tbkj:Anand%40334@192.168.1.43:5432/storeman",
+      url: dbUrl,
       entities: [UserEntity, TokenEntity, PasswordResetEntity],
       synchronize: true,
     })
