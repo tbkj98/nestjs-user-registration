679624e12b46496c53370d93b22a15a877963a6a
Anand Sharma <anandsharma334@gmail.com>
2021-06-05 22:42:47 +0530

    Remove redundant dependency

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-const parseDbUrl = require('parse-database-url');
 import { PasswordResetEntity } from 'src/entity/reset.entity';
 import { TokenEntity } from '../../entity/token.entity';
 import { UserEntity } from '../../entity/user.entity';
@@ -9,7 +8,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 const dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://tbkj:Anand%40334@192.168.1.43:5432/storeman"
-console.error("Database credentials", parseDbUrl(dbUrl));
+console.error("Database credentials", dbUrl);
 @Module({
   imports: [
     UserModule,
