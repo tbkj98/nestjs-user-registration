086da9122da76fb735cb21a98e62ea6160a5f17a
Anand Sharma <anandsharma334@gmail.com>
2021-06-05 22:39:48 +0530

    Updated Typeorm config

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+const parseDbUrl = require('parse-database-url');
+import { PasswordResetEntity } from 'src/entity/reset.entity';
 import { TokenEntity } from '../../entity/token.entity';
 import { UserEntity } from '../../entity/user.entity';
 import { UserModule } from '../user/user.module';
-import { PasswordResetEntity } from 'src/entity/reset.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 const dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://tbkj:Anand%40334@192.168.1.43:5432/storeman"
-console.error("Database URL", dbUrl);
+console.error("Database credentials", parseDbUrl(dbUrl));
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRoot({
+      type: "postgres",
       url: dbUrl,
       entities: [UserEntity, TokenEntity, PasswordResetEntity],
       synchronize: true,
